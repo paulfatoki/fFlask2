55b0afe9f5af868aa1f087402f84451c1b9700c2
RukmalSenavirathne <s.rukmal@gtngroup.com>
2024-02-25 18:26:46 +0530

    demo flask app deployment in kubernetes.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8-slim
+
+WORKDIR /usr/app
+
+COPY main.py ./
+COPY templates templates/
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt
+EXPOSE 5001
+
+# Run main.py when the container launches
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("result.html", result=result)
+
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    # app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5001)
